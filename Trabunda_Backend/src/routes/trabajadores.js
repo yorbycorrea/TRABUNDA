c5e2952fa84c92b5e8f4998916c7013c2f98929f
Yorby Noel Correa Pacherres <yorbyaries.98@gmail.com>
2025-12-22 08:54:28 -0500

    fix: Se añadio los edpoints GET/PUT/PATCH Y DELETE en trabajadores

--- Trabunda_Backend/src/routes/trabajadores.js
+++ Trabunda_Backend/src/routes/trabajadores.js
@@ -51,5 +51,149 @@ router.post("/", async (req, res) => {
   }
 });
 
+
+// ====================================
+// GET /trabajadores/:id -> Se obtiene por id
+// ====================================
+
+router.get("/id", async(req, res) => {
+  const {id} = req.params;
+
+  try {
+    const [ rows ] = await pool.query("SELECT id, codigo, nombre_completo, dni, sexo, activo FROM trabajadores WHERE id = ?", [id]);
+    if (rows.length === 0){
+      return res.status(404).json({error: "Trabajador no encontrado"});
+    }
+
+    res.json(rows[0]);
+  }catch(err){
+    console.error("Error al obtener trabajador: ", err);
+    res.status(500).json({error: "Error al obtener trabajador"});
+  }
+
+});
+
+// ================================================
+// PUT /trabajadores/:id -> actualizar campos 
+// ================================================
+
+
+router.put("/:id", async (req, res) => {
+  const {id} = req.params;
+  const {nombre_completo, dni, sexo, activo} = req.body;
+
+  const fields = [];
+  const values = [];
+
+  if (nombre_completo != undefined){
+    fields.push("nombre completo = ?");
+    values.push(nombre_completo);
+
+  }
+
+  if (dni != undefined){
+    fields.push("dni = ?");
+    values.push(dni);
+  }
+
+  if(sexo != undefined){
+    fields.push("sexo = ?");
+    values.push(sexo);
+  }
+
+  if(activo != undefined){
+    fields.push("activo = ?");
+    values.push(activo);
+  }
+  if (fields.length === 0){
+    return res
+      .status(400)
+      .json({error: "No hay campos para actualizar"});
+  }
+
+  try {
+    const [exists ] = await pool.query(
+      "SELECT id FROM trabajadores  WHERE id = ?", [id]
+    );
+
+    if (exists.length === 0){
+      return res.status(404).json({error: "Trabajador no encontrado"});
+
+    }
+
+    const query = `UPDATE trabajadores SET ${fields.join(", ")} WHERE id = ? `;
+    values.push(id);
+
+    await pool.query(query, values);
+
+    res.json({message: "Trabajador actualizado correctamente"});
+
+
+  } catch(err){
+    console.error("Error al actualizar trabajador:", err);
+    res.status(500).json({error: "Error al actualizar trabajador"});
+  }
+});
+
+// ============================================
+// DELETE /trabajadores/:id  -> dar de baja o eliminar
+// ============================================
+
+router.delete("/id", async(req, res) => {
+  const{id} = req.params;
+
+  try {
+    const [exists] = await pool.query("SELECT id FROM trabajadores WHERE id = ?", [id]);
+
+    if(exists.length === 0){
+      return res.status(404).json({error: "Trabajador no encontrado"});
+    }
+
+    await pool.query("UPDATE trabajadores SET activo = 0 WHERE id = ?", [id]);
+
+    res.json({message: "Trabajador dado de baja correctamente "});
+
+
+  }catch(err){
+    console.error("Error al dar de bajar al trabajador", err);
+    res.status(500).json({error: "Error al dar de bajar al trabajador"});
+  }
+
+  
+
+});
+
+//================================================
+// PATCH /trabajadores/:id/activar -> activar
+// ===============================================
+
+router.patch("/:id/activar", async(req, res) =>{
+  const {id} = req.params;
+
+  try {
+
+    const [exists] = await pool.query(
+      "SELECT id FROM trabajadores WHERE id = ?", [id]
+    );
+
+    if (exists.length === 0){
+      return res.status(404).json({error: "Trabajador no encontrado"});
+
+    }
+
+    await pool.query("UPDATE trabajadores SET activo = 1 WHERE id = ?", [id]),
+
+    res.json({message: "Trabajador activo correctamente "});
+
+
+  }catch(err){
+    console.error("Error al activar trabajador" , err);
+    res.status(500).json({error: "Error al activar trabajador"});
+  }
+})
+
+
+
+
 // exportar SOLO el router
 module.exports = router;
